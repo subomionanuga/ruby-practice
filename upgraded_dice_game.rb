853091d8acecbf771a2cd5747187c6f135f8abd4
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-11-08 13:07:03 +0000

    Dice Game Challenge

---
+++ upgraded_dice_game.rb
@@ -0,0 +1,10 @@
+player_1 = rand(7) + rand(7)
+player_2 = rand(7) + rand(7)
+
+if player_1 > player_2
+  puts "You win"
+else
+  puts "You lose"
+end
+#Need to figure out how to upgrade the game once more: now we play several rounds for each game.
+#The first player to win two rounds wins the game. (Still with two dice each.)
